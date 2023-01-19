a26963da9f8d08513c3f01e6eff2563b0891ea48
DarkestMatter <chotu50505@gmail.com>
2023-01-19 12:58:13 +0530

    mongo DB atlas cloud connection established

--- digi_app/src/db.ts
+++ digi_app/src/db.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 
 // const get_line_no = () => {
 //   var obj = {};
@@ -7,8 +7,14 @@ import mongoose from "mongoose";
 // };
 
 export const connect_db = () => {
+  const uri =
+    "mongodb+srv://<userName>:<pwd>@cluster0.tjhxona.mongodb.net/digi_cheque?retryWrites=true&w=majority";
   try {
-    mongoose.connect("mongodb://127.0.0.1/digi_db");
+    //mongoose.connect("mongodb://127.0.0.1/digi_db");
+    mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    } as ConnectOptions);
     mongoose.Promise = require("bluebird");
   } catch (err) {
     console.log(err);
