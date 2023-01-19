c002af34fcc6390fdddc2f34d76e72029d71335f
mahesh <deshmukh.mahesh.n@gmail.com>
2023-01-19 20:43:22 +0530

    feat: added create and get check data

---
+++ digi_app/src/controller/createcheque/getchequhistory.ts
@@ -0,0 +1,27 @@
+import { RequestHandler } from "express";
+import { createChequeModel } from "../../model/createcheque/createchequerequest";
+
+export const getchequhistory: RequestHandler = async (req, res, next) => {
+  try {
+    if (!req.params.id) {
+      throw new Error("Invalid request");
+    }
+    console.log("I am here.....");
+
+    const checkhistory = await createChequeModel().find({
+      userid: req.params.id,
+    });
+    if (!checkhistory.length) {
+      res.status(204).json({
+        success: true,
+        message: `No checkhistory found with the id of ${req.params.id}`,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      data: checkhistory,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
--- digi_app/src/routes/router.ts
+++ digi_app/src/routes/router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { bankUserLogin } from "../controller/bank/login/bankUserLogin";
 import { getUserDetails } from "../controller/login/getUserDetails";
+import { createchequerequest } from "../controller/createcheque/createchequerequest";
+import { getchequhistory } from "../controller/createcheque/getchequhistory";
 
 export const router = Router();
 
@@ -9,4 +11,10 @@ router.post("/getUserLogin", (req, res, next) => {
 });
 router.post("/bankUserLogin", (req, res, next) => {
   bankUserLogin(req, res, next);
+});
+router.post("/createcheque", (req, res, next) => {
+  createchequerequest(req, res, next);
+});
+router.post("/getchequhistory/:id", (req, res, next) => {
+  getchequhistory(req, res, next);
 });
