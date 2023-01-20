e9a453b463877f3d7a254df4731ed1d8d4af08c1
mahesh <deshmukh.mahesh.n@gmail.com>
2023-01-20 19:24:20 +0530

    feat: added logic to protect rout

--- digi_web/src/slices/saga/handleCreateCheque.ts
+++ digi_web/src/slices/saga/handleCreateCheque.ts
@@ -10,6 +10,7 @@ import {
 import { RootState } from "../reducers";
 import { SagaIterator } from "redux-saga";
 import { ICreateChequeRequest } from "../../interfaces/Cheque/CreateChequeRequest";
+import { getLoggedInUserEmailSelector } from "../../selectors/getLoggedInUserEmailSelector";
 
 export function* handleCreateCheque(
   action: ReturnType<typeof handleCreateChequeRequest>
@@ -19,9 +20,9 @@ export function* handleCreateCheque(
     const state: RootState = yield select();
     const { amount, name, chequeClearanceDate, bankId, mobileNumber, email } =
       state.createCheque.createChequeForm;
-    //@ts-ignore
+    const userId = yield select(getLoggedInUserEmailSelector);
     const request: ICreateChequeRequest = {
-      userid: "1002",
+      userid: userId,
       amount,
       name,
       chequeClearanceDate,
@@ -29,12 +30,12 @@ export function* handleCreateCheque(
       mobileNo: mobileNumber,
       email,
     };
-    const response = yield call(api.chequeRequest.createCheque,request)
-     if(response?.data && response.status === 200){
-      yield put(saveCurrentTransactionDetails(response.data.data))
-      yield put(shouldShowCreateChequePopup(false))
-      yield put(resetCreateChequeFormData())
-      action.payload.navigate('/banklogin')
+    const response = yield call(api.chequeRequest.createCheque, request);
+    if (response?.data && response.status === 200) {
+      yield put(saveCurrentTransactionDetails(response.data.data));
+      yield put(shouldShowCreateChequePopup(false));
+      yield put(resetCreateChequeFormData());
+      action.payload.navigate("/banklogin");
     }
   } catch (error) {
   } finally {
