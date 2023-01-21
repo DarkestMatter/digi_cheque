8f8e71ea4852344669d69cb447130659915fa16c
mahesh <deshmukh.mahesh.n@gmail.com>
2023-01-21 10:41:43 +0530

    feat: check history table scroll bar chnage

--- digi_web/src/components/dashboard/chequeHistory/ChequeHistoryData.tsx
+++ digi_web/src/components/dashboard/chequeHistory/ChequeHistoryData.tsx
@@ -1,4 +1,4 @@
-import { TableBody, TableCell, TableRow } from "@mui/material";
+import { Button, TableBody, TableCell, TableRow } from "@mui/material";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store";
 import { getBankName } from "../../../utils/getBanckName";
@@ -6,27 +6,57 @@ export const ChequeHistoryData = () => {
   const chequeHistory = useSelector(
     (state: RootState) => state.createCheque.chequeHistory
   );
+  const getButtonColor = (
+    chequeStatus?: string | null
+  ):
+    | "inherit"
+    | "primary"
+    | "secondary"
+    | "success"
+    | "error"
+    | "info"
+    | "warning" => {
+    switch (true) {
+      case chequeStatus === "Open":
+        return "info";
+      case chequeStatus === "Authorized":
+        return "secondary";
+      default:
+        return "primary";
+    }
+  };
   return (
     <>
       <TableBody>
-        {chequeHistory.map((chequesDetails, index) => (
-          <TableRow
-            key={chequesDetails.name}
-            sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-          >
-            <TableCell component="th" scope="row">
-              {index + 1}
-            </TableCell>
-            <TableCell align="right">{chequesDetails.name}</TableCell>
-            <TableCell align="right">{chequesDetails.mobileNo}</TableCell>
-            <TableCell align="right">{chequesDetails.email}</TableCell>
-            <TableCell align="right">{getBankName(chequesDetails?.bankName)}</TableCell>
-            <TableCell align="right">{chequesDetails?.createdDate}</TableCell>
-            <TableCell align="right">{chequesDetails?.updatedDate}</TableCell>
-            <TableCell align="right">{chequesDetails?.transactionId}</TableCell>
-            <TableCell align="right">{chequesDetails?.chequeStatus}</TableCell>
-          </TableRow>
-        ))}
+        {chequeHistory.map((chequesDetails, index) => {
+          const buttonColor = getButtonColor(chequesDetails?.chequeStatus);
+          return (
+            <TableRow
+              key={chequesDetails.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {index + 1}
+              </TableCell>
+              <TableCell align="left">{chequesDetails.name}</TableCell>
+              <TableCell align="left">{chequesDetails.mobileNo}</TableCell>
+              <TableCell align="left">{chequesDetails.email}</TableCell>
+              <TableCell align="left">
+                {getBankName(chequesDetails?.bankName)}
+              </TableCell>
+              <TableCell align="left">{chequesDetails?.createdDate}</TableCell>
+              <TableCell align="left">{chequesDetails?.updatedDate}</TableCell>
+              <TableCell align="left">
+                {chequesDetails?.transactionId}
+              </TableCell>
+              <TableCell align="left">
+                <Button color={buttonColor}>
+                  {chequesDetails?.chequeStatus}
+                </Button>
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </>
   );
