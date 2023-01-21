49ebb0fd288a23dc65f549803c620d9748b30087
mahesh <deshmukh.mahesh.n@gmail.com>
2023-01-21 09:30:48 +0530

    added small change in nav bar

---
+++ digi_web/src/components/dashboard/Navbar/index.tsx
@@ -0,0 +1,104 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getLoggedInUserInitialSelector } from "../../../selectors/getLoggedInUserInitialSelector";
+import { settings } from "../../../utils/settings";
+import { setIsAuthenticated } from "../../../slices/userDetail/userSlice";
+import { isUserAuthenticatedSelector } from "../../../selectors/isUserAuthenticatedSelector";
+import { Logo } from "./Logo";
+
+export const Navbar: React.FC = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleSettings = (setting: string) => {
+    handleCloseUserMenu();
+    if (setting === "Profile") {
+      navigate("/profile");
+    }
+    if (setting === "Home") {
+      navigate("/dashboard");
+    }
+    if (setting === "Logout") {
+      dispatch(
+        setIsAuthenticated({
+          isAuthenticated: false,
+          loggedInUserEmail: null,
+        })
+      );
+    }
+  };
+
+  const userInitial = useSelector(getLoggedInUserInitialSelector);
+  const isAuthenticated = useSelector(isUserAuthenticatedSelector);
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Logo/>
+          {isAuthenticated && (
+            <>
+              <Box
+                sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+              ></Box>
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt={userInitial}
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem
+                      key={setting}
+                      onClick={() => handleSettings(setting)}
+                    >
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
